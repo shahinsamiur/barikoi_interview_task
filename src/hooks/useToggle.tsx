9370bb736033e934fa5a16cd10787d20e95daf19
shahinsamiur <shahinsamiur@yahoo.com>
2026-04-26 01:44:26 +0600

    refactor: less important hook removed

--- src/hooks/useToggle.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export function useToggle(initial = false) {
-  const [state, setState] = useState(initial);
-
-  const toggle = () => setState((prev) => !prev);
-
-  return { state, toggle, setState };
-}
