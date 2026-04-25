2368ef064fd066fdeb9c249b4ce143eb868693b9
shahinsamiur <shahinsamiur@yahoo.com>
2026-04-25 14:04:32 +0600

    feat: wire SearchBar, MapView and LocationCard into main page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,32 @@
 "use client";
-import { toast } from "react-toastify";
-import { Button } from "@/src/utils";
-import { ContainerWraper } from "@/src/utils";
 
-export default function page() {
+import { ContainerWraper } from "@/src/utils";
+import SearchBar from "@/src/components/SearchBar";
+import MapView from "@/src/components/MapView";
+import LocationCard from "@/src/components/LocationCard";
+
+export default function Page() {
   return (
     <ContainerWraper>
-      <div className="min-h-screen">
-        <h1>hello samiur shahin</h1>
-        <Button onClick={() => toast.success("some one clicked me ")}>
-          click me
-        </Button>
+      <div className="min-h-screen flex flex-col gap-4 py-6">
+        {/* Title */}
+        <div>
+          <h1 className="text-2xl font-bold text-gray-800">Location Finder</h1>
+          <p className="text-sm text-gray-500 mt-1">
+            Search any place in Bangladesh and view it on the map
+          </p>
+        </div>
+
+        {/* Search Bar */}
+        <SearchBar />
+
+        {/* Selected Location Info Card */}
+        <LocationCard />
+
+        {/* Map */}
+        <div className="w-full h-125 rounded-xl overflow-hidden">
+          <MapView />
+        </div>
       </div>
     </ContainerWraper>
   );
